497c415f83b65d1205a6d0f1f751de001f323b4f
Magiarvlad <32353058+Magiarvlad@users.noreply.github.com>
2018-10-15 20:39:13 +0300

    Add files via upload

---
+++ Ovidiu/Ovidiu/Frm_Pornire.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Ovidiu.Miscellaneous;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ovidiu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Frm_Pornire : Window
+    {
+        public Frm_Pornire()
+        {
+            InitializeComponent();
+
+
+            if( ClasaSuport.ProgramIsAlreadyRunning() )
+            {
+                MessageBox.Show( "Aplicatia ruleaza deja", "Eroare", MessageBoxButton.OK);
+            }
+        }
+    }
+}
---
+++ Ovidiu/Ovidiu/Miscellaneous/ClasaSuport.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovidiu.Miscellaneous
+{
+    public static class ClasaSuport
+    {
+
+        public static bool ProgramIsAlreadyRunning()
+        {
+            string procName = Process.GetCurrentProcess().ProcessName;
+
+            // get the list of all processes by the "procName"       
+            Process[] processes = Process.GetProcessesByName(procName);
+
+            if (processes.Length > 1)
+            {
+               
+                return true;
+            }
+            else
+            {
+                return false;
+                // Application.Run(...);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+    
+}
