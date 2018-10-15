83f0af69f5fe8a28be4da4874d6dcc72750ca52e
Grad Ioan <ionut.grad95@gmail.com>
2018-10-15 21:01:34 +0300

    Update Ovidiu/Ovidiu/Miscellaneous/ClasaSuport.cs
    IoGr

--- Ovidiu/Ovidiu/Miscellaneous/ClasaSuport.cs
+++ Ovidiu/Ovidiu/Miscellaneous/ClasaSuport.cs
@@ -13,7 +13,6 @@ namespace Ovidiu.Miscellaneous
         public static bool ProgramIsAlreadyRunning()
         {
             string procName = Process.GetCurrentProcess().ProcessName;
-            // get the list of all processes by the "procName"       
             Process[] processes = Process.GetProcessesByName(procName);
 
             if (processes.Length > 1)
