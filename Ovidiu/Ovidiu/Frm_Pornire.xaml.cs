3e7f9e083db55a753cb81a9b97c1db09275510bf
Magiarvlad <32353058+Magiarvlad@users.noreply.github.com>
2018-10-21 19:00:45 +0300

    VL

--- Ovidiu/Ovidiu/Frm_Pornire.xaml.cs
+++ Ovidiu/Ovidiu/Frm_Pornire.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Deployment.Application;
 using System.Reflection;
-
+using Ovidiu.EU;
 namespace Ovidiu
 {
     /// <summary>
