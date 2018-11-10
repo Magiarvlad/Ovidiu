59b7208412a212b522092fe40108de2b57021563
Grad Ioan <ionut.grad95@gmail.com>
2018-11-10 22:01:06 +0200

    New FRM_Main

--- Ovidiu/Ovidiu/obj/Debug/Ovidiu_Content.g.i.cs
+++ Ovidiu/Ovidiu/obj/Debug/Ovidiu_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("imagini/curstext.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("imagini/menu/logo.png")]
 
 
