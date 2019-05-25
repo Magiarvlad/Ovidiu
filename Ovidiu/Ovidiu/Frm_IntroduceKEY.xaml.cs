6ddc12532895fcd544d1cacff1fa970fca0b9bbb
Magiarvlad <32353058+Magiarvlad@users.noreply.github.com>
2019-05-25 23:01:50 +0300

    RegKey

--- Ovidiu/Ovidiu/Frm_IntroduceKEY.xaml.cs
+++ Ovidiu/Ovidiu/Frm_IntroduceKEY.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Ovidiu.EU;
+using Ovidiu.Modules;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +31,64 @@ namespace Ovidiu
         {
             Frm_VizualizareKEY frm_VizualizareKEY = new Frm_VizualizareKEY();
             frm_VizualizareKEY.Show();
+        }
+
+        private void BtnValidare_Click(object sender, RoutedEventArgs e)
+        {
+            if(TxtKey.Text=="")
+            {
+                MessageBox.Show("Campul Chei de inregistrare nu poate fi gol");
+
+            }
+            else
+            {
+                try { 
+                string txtkey = TxtKey.Text.Replace('\r',' ') ;
+                string[] keys = txtkey.Split('\n');
+                
+                for(int i=keys.Length-1; i>=0; i--)
+                {
+                    if (keys[i] != " " && keys[i] != "" && keys[i] != null)
+                    keys[i] =keys[i].Trim();
+                }
+                for (int i = keys.Length - 1; i >= 0; i--)
+                {
+                    string[] arrKeyTxt = new string[4];
+
+                    if (keys[i].Length > 17)
+                    {
+                        arrKeyTxt = Inregistrare.DecodeKey(keys[i]);
+                        if(arrKeyTxt[0]==CodFiscal.Text.Trim())
+                        {
+                           
+                            WriteInFile(keys[i], CodFiscal.Text, arrKeyTxt[2]);
+                            MessageBox.Show(string.Format("Cheia: {0} a fost inregistrata cu success!", keys[i]));
+                        }
+                        else
+                        {
+                            MessageBox.Show(string.Format("Cheia: {0} era INVALIDA sau Codul fiscal nu coincide!", keys[i]));
+                        }
+                    }
+
+                }
+                }
+                catch
+                {
+                    MessageBox.Show(string.Format("Intregistrarea a esuat!"));
+                }
+            }
+        }
+
+        private void WriteInFile(string v1, string text, string v2)
+        {
+       
+            string[] vs = new string[3];
+            StreamWriter stream = File.AppendText(FileLocation.System + "key\\chei.txt");
+
+            stream.WriteLine(v1+"\t"+text + "\t" + v2);
+            
+
+            stream.Close();
         }
     }
 }
--- Ovidiu/Ovidiu/Modules/Inregistrare.cs
+++ Ovidiu/Ovidiu/Modules/Inregistrare.cs
@@ -1,17 +1,74 @@
-﻿using System;
+﻿using Ovidiu.EU;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Ovidiu.Modules
 {
-    class Inregistrare
+    public class Inregistrare
     {
         public static bool Verifica_Fisier(string cod_fiscal)
         {
 
             return true;
+        }
+        public static string[] DecodeKey (string a)
+          {
+            string[] vs = new string[4];
+            try {
+            
+            string CF = a.Substring(2,8);
+            CF = ((Convert.ToUInt64(CF)- 28061973)/2).ToString();
+            CF = "RO" + CF;
+            int gratuit = Convert.ToInt32(a.Substring(10, 1));
+            int nr = Convert.ToInt32(a.Substring(11, 2));
+            int Anul = Convert.ToInt32(a.Substring(13)) / nr;
+
+            vs[0] = CF;
+            vs[1] = gratuit.ToString();
+            vs[2] = Anul.ToString();
+            vs[3] = "1";
+            }
+            catch
+            {
+                vs[0] = "";
+                vs[1] = "";
+                vs[2] = "";
+                vs[3] = "0";
+            }
+
+            return vs;
+          }
+
+        public static void VerificaKeyAnul()
+        {
+            string[] keys;
+            string line;
+            string[] vs = new string[3];
+            StreamReader stream = new StreamReader(FileLocation.System + "key\\chei.txt");
+            // FileStream fisier = new FileStream( FileLocation.System+"key\\chei.txt", FileMode.Open, FileAccess.ReadWrite);
+            int i=0;
+            while(stream.ReadLine()!=null)
+            {
+                i++;
+            };
+            keys = new string[i];
+
+            int j = 0;
+            while ( (line=stream.ReadLine()) != null)
+            {
+                vs = line.Split(' ');
+                if(vs[1]==Firma.CodFiscal)
+                {
+                    keys[j] = line;
+                    j++;
+                }
+            }
+
+            stream.Close();
         }
     }
 }
